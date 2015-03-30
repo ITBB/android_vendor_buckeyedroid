5c35c236cfe4f3d120752782364b3ee9abd918c3
Jeremy <jmeade300@gmail.com>
2015-03-30 08:32:36 -0400

    Add missing audio lines and push other random stuff not used yet

--- products/buckeyedroid_shamu.mk
+++ products/buckeyedroid_shamu.mk
@@ -17,9 +17,20 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT=google/shamu/shamu:5.1/LMY47E/1748839:user/release-keys \
     PRIVATE_BUILD_DESC="shamu-user 5.1 LMY47E 1748839 release-keys"
 
+#fmas n6 specific
+    PRODUCT_PROPERTY_OVERRIDES += \
+    fmas.spkr_6ch=35,20,110 \
+    fmas.spkr_2ch=35,25 \
+    fmas.spkr_angles=10 \
+    fmas.spkr_sgain=0 \
+    media.aac_51_output_enabled=true \
+    persist.rcs.supported=0 \
+    persist.radio.sib16_support=1 \
+    drm.service.enabled=true \
+    persist.audio.dualmic.config=endfire \
+    persist.audio.fluence.voicecall=true \
+    persist.audio.fluence.voicerec=false \
+    persist.audio.fluence.speaker=false 
+
 # Inherit media effect blobs
 -include vendor/buckeyeddroid/products/common_media_effects.mk
-
-# Enable tethering
-PRODUCT_PROPERTY_OVERRIDES += \
-net.tethering.noprovisioning=true
